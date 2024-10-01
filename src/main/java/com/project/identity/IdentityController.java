936f915b985291905f5db0c317a01993428b208d
Grover <grovergonzalez8@gmail.com>
2024-10-01 18:45:58 -0400

    adding implementations

--- src/main/java/com/project/identity/IdentityController.java
+++ src/main/java/com/project/identity/IdentityController.java
@@ -1,8 +1,23 @@
 package com.project.identity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IdentityController {
+    @Autowired
+    private IdentityService identityService;
 
+    @PostMapping("/add")
+    public String addCI(@RequestBody String ci) {
+        identityService.addCI(ci);
+        return "CI added: " + ci;
+    }
+
+    @PostMapping("/verify")
+    public boolean verifyCI(@RequestBody IdentityDto identityDto) {
+        return identityService.verifyCI(identityDto);
+    }
 }
--- src/main/java/com/project/identity/IdentityService.java
+++ src/main/java/com/project/identity/IdentityService.java
@@ -1,4 +1,19 @@
 package com.project.identity;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class IdentityService {
+    private List<String> identityList = new ArrayList<>();
+
+    public void addCI(String ci) {
+        identityList.add(ci);
+    }
+
+    public boolean verifyCI(IdentityDto identityDto) {
+        return identityList.contains(identityDto.getCi()); // Verifica si el CI está en la lista
+    }
 }
